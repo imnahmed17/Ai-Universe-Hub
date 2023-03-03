f7fb7e3896ad022d216b7a2be63d66eb9ffbc4b2
helloashik17 <asiknoman150@gmail.com>
2023-03-03 20:49:23 +0600

    card footer added

--- js/ai.js
+++ js/ai.js
@@ -1,11 +1,11 @@
-const loadAiCards = async() => {
+const loadAiCards = async(dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    displayAiCards(data.data.tools);
+    displayAiCards(data.data.tools, dataLimit);
 }
 
-const displayAiCards = (aiCards) => {
+const displayAiCards = (aiCards, dataLimit) => {
     // console.log(aiCards);
     // display all ai cards
     const aiContainer = document.getElementById('ai-container');
@@ -15,12 +15,23 @@ const displayAiCards = (aiCards) => {
         aiDiv.classList.add('col');
         aiDiv.innerHTML = `
         <div class="card h-100 p-4">
-            <img src="${aiCard.image}" class="card-img-top" alt="..." style="height: 50%;">
+            <img src="${aiCard.image}" class="card-img-top rounded-3" alt="..." style="height: 50%;">
             <div class="card-body h-25">
                 <h5 class="card-title fw-bold">Features</h5>
                 <p>${aiCard.features.map(feature => `<li type="1" class="text-dark-emphasis">${feature}</li>`).join('')}</p>
             </div>
-            
+            <div class="card-body h-25">
+                <hr>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5 class="card-title fw-bold">${aiCard.name}</h5>
+                        <p><i class="bi bi-calendar4-week"></i>&nbsp; ${aiCard.published_in}</p>
+                    </div>
+                    <div>
+                        <button  href="#" class="border-0 rounded-circle px-2 py-1 bg-danger bg-opacity-10" data-bs-toggle="modal" data-bs-target="#aiCardDetailModal"><i class="bi bi-arrow-right text-danger"></i></button>
+                    </div>
+                </div>
+            </div>
         </div>
         `;
         aiContainer.appendChild(aiDiv);
