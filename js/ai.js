66d9a5814e9887cdd8f8961b19a576b4aa926d79
helloashik17 <asiknoman150@gmail.com>
2023-03-03 20:36:03 +0600

    card image and features added

--- js/ai.js
+++ js/ai.js
@@ -6,7 +6,25 @@ const loadAiCards = async() => {
 }
 
 const displayAiCards = (aiCards) => {
-    console.log(aiCards);
+    // console.log(aiCards);
+    // display all ai cards
+    const aiContainer = document.getElementById('ai-container');
+    aiContainer.textContent = '';
+    aiCards.forEach(aiCard => {
+        const aiDiv = document.createElement('div');
+        aiDiv.classList.add('col');
+        aiDiv.innerHTML = `
+        <div class="card h-100 p-4">
+            <img src="${aiCard.image}" class="card-img-top" alt="..." style="height: 50%;">
+            <div class="card-body h-25">
+                <h5 class="card-title fw-bold">Features</h5>
+                <p>${aiCard.features.map(feature => `<li type="1" class="text-dark-emphasis">${feature}</li>`).join('')}</p>
+            </div>
+            
+        </div>
+        `;
+        aiContainer.appendChild(aiDiv);
+    });
 }
 
 
